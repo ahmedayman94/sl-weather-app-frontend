0c733f20c993e5170c44a0817d528d66f81db454
ahmedayman94 <ahmedmou@kth.se>
2020-09-01 22:05:08 +0200

    get basic momentjs times to be sent to api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { SlInfoComponent } from './main-page/sl-info/sl-info.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from './mat.module';
+import { MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,9 @@ import { MaterialModule } from './mat.module';
     HttpClientModule,
     MaterialModule
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: { useUtc: true } }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
