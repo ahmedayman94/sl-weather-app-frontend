b50eef9592379eddcb8211297e9fbfd158db6228
ahmedayman94 <ahmedmou@kth.se>
2020-12-01 22:49:25 +0100

    changed base ref and endpoints for api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,8 @@
 export const environment = {
   production: true,
-  localSLApiUrl: "http://localhost:4201/api/sl/realtimedeparturesV4.json",
+  localSLApiUrl: "api/sl",
   slApiUrl: "https://api.sl.se/api2/realtimedeparturesV4.json",
-  slApiKey: "<sl-api-key>",
-  localWeatherApiUrl: "/v2.0/forecast/hourly",
+  localWeatherApiUrl: "api/weatherbit",
   weatherApiUrl: "https://api.weatherbit.io/v2.0/forecast/hourly",
-  weatherApiKey: "<Weather-api-key>"
+  localClimacellApiUrl: "api/climacell",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,9 @@
 
 export const environment = {
   production: false,
-  localSLApiUrl: "http://localhost:4201/api/sl/realtimedeparturesV4.json",
-  slApiUrl: "https://api.sl.se/api2/realtimedeparturesV4.json",
-  slApiKey: "<sl-api-key>",
+  localSLApiUrl: "",
   localWeatherApiUrl: "/v2.0/forecast/hourly",
-  weatherApiUrl: "https://api.weatherbit.io/v2.0/forecast/hourly",
+  localClimacellApiUrl: "./assets/weather-climacell-mockdata.json",
   weatherApiKey: "<Weather-api-key>"
 };
 
